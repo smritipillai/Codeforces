8722865eb34c9b97c7f5ad4443f1e04504056edb
smriti_pillai <smritipillai.smriti@gmail.com>
2018-06-04 13:51:16 +0530

    codeforces

---
+++ fafa.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,cnt=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        cnt++;
+    }
+    printf("%d",cnt);
+    return 0;
+}

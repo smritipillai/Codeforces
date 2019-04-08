9b2b0af502c9beaea6deab532a7cf7e14ea78810
Smriti <smritipillai.smriti@gmail.com>
2019-04-08 18:10:19 +0530

    Codeforces Div 2a

---
+++ PetyaAndStrings.c
@@ -0,0 +1,10 @@
+// Question link: https://codeforces.com/problemset/problem/112/A
+//Codeforces Div 2a
+
+#include<stdio.h>
+int main(){
+char a[100],b[100];
+gets(a);
+gets(b);
+printf("%d",strcasecmp(a,b));
+}
